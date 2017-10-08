b8f8664a16b9d8cfd62833870ddb373340d175f2
Alexander Clairmont <clairmoa@flip1.engr.oregonstate.edu>
2017-10-08 11:15:04 -0700

    changed dominion.c per assignment 2 criteria

--- projects/clairmoa/dominion/dominion.c
+++ projects/clairmoa/dominion/dominion.c
@@ -643,53 +643,37 @@ int getCost(int cardNumber)
   return -1;
 }
 
-int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState *state, int handPos, int *bonus)
-{
-  int i;
-  int j;
-  int k;
-  int x;
-  int index;
-  int currentPlayer = whoseTurn(state);
-  int nextPlayer = currentPlayer + 1;
-
-  int tributeRevealedCards[2] = {-1, -1};
-  int temphand[MAX_HAND];// moved above the if statement
-  int drawntreasure=0;
-  int cardDrawn;
-  int z = 0;// this is the counter for the temp hand
-  if (nextPlayer > (state->numPlayers - 1)){
-    nextPlayer = 0;
-  }
-  
-	
-  //uses switch to select card and perform actions
-  switch( card ) 
-    {
-    case adventurer:
-      while(drawntreasure<2){
+int adventurerCard(int currentPlayer, struct gameState *state) {
+	int temphand[MAX_HAND];// moved above the if statement
+	int drawntreasure=0;
+	int cardDrawn;
+	int z = 0;// this is the counter for the temp hand
+	while(drawntreasure<2){
 	if (state->deckCount[currentPlayer] <1){//if the deck is empty we need to shuffle discard and add to deck
-	  shuffle(currentPlayer, state);
+		shuffle(currentPlayer, state);
 	}
 	drawCard(currentPlayer, state);
 	cardDrawn = state->hand[currentPlayer][state->handCount[currentPlayer]-1];//top card of hand is most recently drawn card.
 	if (cardDrawn == copper || cardDrawn == silver || cardDrawn == gold)
-	  drawntreasure++;
+		drawntreasure++;
 	else{
-	  temphand[z]=cardDrawn;
-	  state->handCount[currentPlayer]--; //this should just remove the top card (the most recently drawn one).
-	  z++;
-	}
-      }
+		temphand[z]=cardDrawn;
+		state->handCount[currentPlayer]--; //this should just remove the top card (the most recently drawn one).
+		z++;
+		}
+    }
       while(z-1>=0){
 	state->discard[currentPlayer][state->discardCount[currentPlayer]++]=temphand[z-1]; // discard all cards in play that have been drawn
-	z=z-1;
+	z=z-10; // changed from 1 to 10
       }
       return 0;
-			
-    case council_room:
-      //+4 Cards
-      for (i = 0; i < 4; i++)
+	  
+}
+int councilRoomCard(int currentPlayer, struct gameState *state, int handPos) {
+    int i;
+	//+4 Cards
+	// starting at 1 and going through 3, less than 4 cards
+      for (i = 1; i < 3; i++)
 	{
 	  drawCard(currentPlayer, state);
 	}
@@ -698,7 +682,7 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
       state->numBuys++;
 			
       //Each other player draws a card
-      for (i = 0; i < state->numPlayers; i++)
+      for (i = 0; i < state->numPlayers*10; i++) // 10 extra cards for everyone else
 	{
 	  if ( i != currentPlayer )
 	    {
@@ -710,6 +694,78 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
       discardCard(handPos, currentPlayer, state, 0);
 			
       return 0;
+}
+int smithyCard(int currentPlayer, struct gameState *state, int handPos) {
+    int i;
+	//+3 Cards
+	// 3 -> 1 cards
+      for (i = 0; i < 1; i++)
+	{
+	  drawCard(currentPlayer, state);
+	}
+      //discard 3 card from hand
+	 for (i = 0; i < 3; i++)
+	{
+	  discardCard(handPos, currentPlayer, state, 0);
+	}
+      
+      return 0;
+}
+int greatHallCard(int currentPlayer, struct gameState *state, int handPos){
+      //+1 Card
+	  
+      drawCard(currentPlayer, state);
+
+	  // -- numActions instead of ++
+      state->numActions--;
+	  
+      //discard card from hand
+      discardCard(handPos, currentPlayer, state, 0);
+      return 0;	
+	
+}
+int villageCard(int currentPlayer, struct gameState *state, int handPos) {
+	   //+1 Card
+      drawCard(currentPlayer, state);
+			
+      //+2 Actions
+	  
+	  // from 2 actions to 20
+      state->numActions = state->numActions + 20;
+			
+      //discard played card from hand
+      discardCard(handPos, currentPlayer, state, 0);
+      return 0;
+}
+
+int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState *state, int handPos, int *bonus)
+{
+  int i;
+  int j;
+  int k;
+  int x;
+  int index;
+  int currentPlayer = whoseTurn(state);
+  int nextPlayer = currentPlayer + 1;
+
+  int tributeRevealedCards[2] = {-1, -1};
+	  int temphand[MAX_HAND];// moved above the if statement
+	  int drawntreasure=0;
+	  int cardDrawn;
+	  int z = 0;// this is the counter for the temp hand
+  if (nextPlayer > (state->numPlayers - 1)){
+    nextPlayer = 0;
+  }
+
+ 	
+  //uses switch to select card and perform actions
+  switch( card ) 
+    {
+    case adventurer:
+		return adventurerCard(currentPlayer,state);
+			
+    case council_room:
+		return councilRoomCard(currentPlayer, state, handPos);
 			
     case feast:
       //gain card with cost up to 5
@@ -829,26 +885,10 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
       return 0;
 		
     case smithy:
-      //+3 Cards
-      for (i = 0; i < 3; i++)
-	{
-	  drawCard(currentPlayer, state);
-	}
-			
-      //discard card from hand
-      discardCard(handPos, currentPlayer, state, 0);
-      return 0;
+		return smithyCard(currentPlayer, state, handPos);
 		
     case village:
-      //+1 Card
-      drawCard(currentPlayer, state);
-			
-      //+2 Actions
-      state->numActions = state->numActions + 2;
-			
-      //discard played card from hand
-      discardCard(handPos, currentPlayer, state, 0);
-      return 0;
+		return villageCard(currentPlayer, state, handPos);
 		
     case baron:
       state->numBuys++;//Increase buys by 1!
@@ -902,15 +942,7 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
       return 0;
 		
     case great_hall:
-      //+1 Card
-      drawCard(currentPlayer, state);
-			
-      //+1 Actions
-      state->numActions++;
-			
-      //discard card from hand
-      discardCard(handPos, currentPlayer, state, 0);
-      return 0;
+		return greatHallCard(currentPlayer, state, handPos);
 		
     case minion:
       //+1 action
